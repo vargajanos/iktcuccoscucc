af963674692280af3d980ac508fa5b57ac1e82b1
vargajanos <80397200+vargajanos@users.noreply.github.com>
2022-04-11 14:11:50 +0200

    hibajavítás

--- menütéglatest.py
+++ menütéglatest.py
@@ -1,8 +1,10 @@
 from tkinter import *
+from tkinter import messagebox
+
 
 def nevjegy():
     abl2= Toplevel(foablak)
-    uz2= Message(abl2, text='Készítette: Gipsz Jakab\nPiripócs\n2009.06.04', width=300)
+    uz2= Message(abl2, text='Készítette: Varga János\Dusnok\n2006.03.14', width=300)
     gomb2= Button(abl2,text="Kilép", command=abl2.destroy)
     uz2.pack()
     gomb2.pack()
@@ -11,13 +13,32 @@ def nevjegy():
 
 def felszin():
     def szamit():
+        var=mezo1.get()
+        if len(var) == 0:
+            messagebox.showerror('Error', 'A mező üres')
+        else:
+            pass
+        var = int(var)
+        var=mezo2.get()
+        if len(var) == 0:
+            messagebox.showerror('Error', 'A mező üres')
+        else:
+            pass
+        var = int(var)
+        var=mezo3.get()
+        if len(var) == 0:
+            messagebox.showerror('Error', 'A mező üres')
+        else:
+            pass
         a = eval(mezo1.get())
         b = eval(mezo2.get())
         c = eval(mezo3.get())
-        felszin= 2*(a*b+a*c+b*c)
-        mezo4.delete(0,END)
-        mezo4.insert(0, str(felszin))
-
+        if a>0 and b>0 and c>0:        
+            felszin= 2*(a*b+a*c+b*c)
+            mezo4.delete(0,END)
+            mezo4.insert(0, str(felszin))
+        else:
+            messagebox.showerror('Error', 'Negatívval vagy nullával nem lehet elvégezni ')
     abl3=Toplevel(foablak)        
     abl3.title('A téglatest felszíne')
     abl3.minsize(width=300, height=100)
@@ -39,17 +60,38 @@ def felszin():
     mezo2.grid(row=2, column=2, sticky=W)
     mezo3.grid(row=3, column=2, sticky=W)
     mezo4.grid(row=5, column=2, sticky=W)
+    gomb2= Button(abl3,text="Kilép", command=abl3.destroy)
+    gomb2.grid(row=4, column=3, sticky=W)
     abl3.mainloop()
 
 def terfogat():
     def szamit():
+        var=mezo1.get()
+        if len(var) == 0:
+            messagebox.showerror('Error', 'A mező üres')
+        else:
+            pass
+        var = int(var)
+        var=mezo2.get()
+        if len(var) == 0:
+            messagebox.showerror('Error', 'A mező üres')
+        else:
+            pass
+        var = int(var)
+        var=mezo3.get()
+        if len(var) == 0:
+            messagebox.showerror('Error', 'A mező üres')
+        else:
+            pass                        
         a = eval(mezo1.get())
         b = eval(mezo2.get())
         c = eval(mezo3.get())
-        terfogat=a*b*c
-        mezo4.delete(0,END)
-        mezo4.insert(0, str(terfogat))
-
+        if a>0 and b>0 and c>0:
+            terfogat=a*b*c
+            mezo4.delete(0,END)
+            mezo4.insert(0, str(terfogat))
+        else:
+            messagebox.showerror('Error', 'Negatívval vagy nullával nem lehet elvégezni ')
     abl3=Toplevel(foablak)        
     abl3.title('A téglatest térfogata')
     abl3.minsize(width=300, height=100)
@@ -71,6 +113,8 @@ def terfogat():
     mezo2.grid(row=2, column=2, sticky=W)
     mezo3.grid(row=3, column=2, sticky=W)
     mezo4.grid(row=5, column=2, sticky=W)
+    gomb2= Button(abl3,text="Kilép", command=abl3.destroy)
+    gomb2.grid(row=4, column=3, sticky=W)    
     abl3.mainloop()
 
 foablak=Tk()
